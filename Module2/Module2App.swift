4b92df642923860ac1efaa83df09ab8c3097d2a6
Matt Barson <matt.barson@gmail.com>
2024-11-02 18:29:31 +0000

    init commit

---
+++ Module2/Module2App.swift
@@ -0,0 +1,17 @@
+//
+//  Module2App.swift
+//  Module2
+//
+//  Created by Matthew Barson on 23/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Module2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Module2Tests/Module2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Module2Tests.swift
+//  Module2Tests
+//
+//  Created by Matthew Barson on 23/09/2024.
+//
+
+import Testing
+@testable import Module2
+
+struct Module2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
